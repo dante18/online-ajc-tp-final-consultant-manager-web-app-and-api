70ea377f770b04f42aa8b9841a48afb2d7908a61
dante18 <lablandinerie2@gmail.com>
2025-10-22 19:37:43 +0200

    feat: add classes in ConsultTechApp.Web.Services used to call api since web

---
+++ ConsultTechApp.Web.Services/Dtos/Application/Customers/CustomerDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace ConsultTechApp.Web.Services.Dtos.Application.Customers;
+
+public class CustomerDto
+{
+    public Guid? Id { get; set; }
+
+    [DisplayName("Customer Name")]
+    public string CompanyName { get; set; }
+
+    [DisplayName("Sector of activity")]
+    public string Industry { get; set; }
+
+    [DisplayName("Address")]
+    public string Address { get; set; }
+
+    [DisplayName("Contact Name")]
+    public string ContactName { get; set; }
+
+    [DisplayName("Contact Email")]
+    public string ContactEmail { get; set; }
+}
