2f9770740a526e446b885353bdd8f01d3e52176f
dante18 <lablandinerie2@gmail.com>
2025-10-22 19:40:33 +0200

    feat: add views link to CustomersController

---
+++ ConsultTechApp.Web/ViewsModels/Customers/CustomersListViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsultTechApp.Web.ViewsModels.Customers;
+
+public class CustomersListViewModel
+{
+    public List<CustomersViewModel> Items { get; set; } = null!;
+}
---
+++ ConsultTechApp.Web/ViewsModels/Customers/CustomersViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsultTechApp.Web.ViewsModels.Customers;
+
+public class CustomersViewModel
+{
+    public Guid? Id { get; set; }
+
+    public string CompanyName { get; set; } = null!;
+
+    public string Industry { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string ContactName { get; set; } = null!;
+
+    public string ContactEmail { get; set; } = null!;
+}
