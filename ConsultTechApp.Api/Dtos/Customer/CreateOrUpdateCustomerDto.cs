3f7c5ca34470be4f0c522ba0b36badb6f654cf1e
dante18 <lablandinerie2@gmail.com>
2025-10-22 19:32:40 +0200

    feat: add customers endpoint

---
+++ ConsultTechApp.Api/Dtos/Customer/CreateOrUpdateCustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsultTechApp.Api.Dtos.Customer;
+
+public class CreateOrUpdateCustomerDto
+{
+    [Required]
+    [StringLength(200)]
+    public string CompanyName { get; set; }
+
+    [Required]
+    public string Industry { get; set; }
+
+    [Required]
+    public string Address { get; set; }
+
+    [Required]
+    public string ContactName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string ContactEmail { get; set; }
+}
---
+++ ConsultTechApp.Api/Dtos/Customer/CustomerDto.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsultTechApp.Api.Dtos.Customer;
+
+public class CustomerDto
+{
+    public Guid? Id { get; set; }
+
+    public string CompanyName { get; set; }
+
+    public string Industry { get; set; }
+
+    public string Address { get; set; }
+
+    public string ContactName { get; set; }
+
+    public string ContactEmail { get; set; }
+}
